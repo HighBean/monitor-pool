c8c08854849fcabaa0b27895847ff343a6f89c44
dapeng <dapeng@wacai.com>
2018-11-28 18:53:19 +0800

    init

---
+++ src/main/java/com/boomboo/monitorpool/MonitorPoolApplication.java
@@ -0,0 +1,12 @@
+package com.boomboo.monitorpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorPoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorPoolApplication.class, args);
+	}
+}
